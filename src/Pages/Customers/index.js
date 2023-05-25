4f5d152160bc0731a1d62be5bed3756e8ed7b9e6
Vijay <yadavajay6826@gmail.com>
2023-05-25 22:27:54 +0530

    first change

--- src/Pages/Customers/index.js
+++ src/Pages/Customers/index.js
@@ -1,7 +1,11 @@
-import { Avatar, Rate, Space, Table, Typography } from "antd";
+import { Avatar, Rate, Card,Space, Table, Typography } from "antd";
 import { useEffect, useState } from "react";
 import { getCustomers, getInventory } from "../../API";
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
 
+ChartJS.register(ArcElement, Tooltip, Legend);
 function Customers() {
   const [loading, setLoading] = useState(false);
   const [dataSource, setDataSource] = useState([]);
@@ -17,6 +21,7 @@ function Customers() {
   return (
     <Space size={20} direction="vertical">
       <Typography.Title level={4}>Customers</Typography.Title>
+      <CustomersCharts/>
       <Table
         loading={loading}
         columns={[
@@ -64,4 +69,72 @@ function Customers() {
     </Space>
   );
 }
+
+function CustomersCharts(){
+
+  const [reveneuData, setReveneuData] = useState({
+    labels: [],
+    datasets: [],
+  });
+
+  useEffect(() => {
+    getCustomers().then((res) => {
+      const labels = res.users.map((users) => {
+        return `User-${users.firstName}`;
+      });
+      const data = res.users.map((users) => {
+        return users.age;
+      });
+ 
+      const dataSource = {
+            labels,
+            datasets: [
+              {
+                label: "Customers",
+                data: data,
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+              },
+            ],
+          
+        
+      };
+
+      setReveneuData(dataSource);
+    });
+  }, []);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom',
+      },
+      title: {
+        display: true,
+        text: 'Customers Review',
+      },
+    },
+  };
+ 
+  return (
+    <Card style={{width:350,height:300}}>
+    <Doughnut options={options} data={reveneuData} />
+    </Card>);
+} 
 export default Customers;
--- src/Pages/Orders/index.js
+++ src/Pages/Orders/index.js
@@ -1,6 +1,10 @@
 import { Avatar, Rate, Space, Table, Typography } from "antd";
 import { useEffect, useState } from "react";
 import { getInventory, getOrders } from "../../API";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function Orders() {
   const [loading, setLoading] = useState(false);
@@ -17,6 +21,7 @@ function Orders() {
   return (
     <Space size={20} direction="vertical">
       <Typography.Title level={4}>Orders</Typography.Title>
+      <OrderCharts/>
       <Table
         loading={loading}
         columns={[
@@ -50,5 +55,69 @@ function Orders() {
       ></Table>
     </Space>
   );
+}
+
+function OrderCharts(){
+
+  const [reveneuData, setReveneuData] = useState({
+    labels: [],
+    datasets: [],
+  });
+
+  useEffect(() => {
+    getOrders().then((res) => {
+      const labels = res.products.map((products) => {
+        return `User-${products.userId}`;
+      });
+      const data = res.products.map((products) => {
+        return products.discountedPrice;
+      });
+
+      const dataSource = {
+        labels,
+        datasets: [
+          {
+            label: 'orders',
+            data: data,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+
+      setReveneuData(dataSource);
+    });
+  }, []);
+
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom',
+      },
+      title: {
+        display: true,
+        text: 'Order Revenue',
+      },
+    },
+  };
+ 
+  return <Pie options={options} data={reveneuData} />;
 }
 export default Orders;
